01bf59ecc7679016deca27dc89ab4b76d6a48f34
Hamza Ghani <h.u.f.ghani@gmail.com>
2016-05-07 20:39:27 +0100

    start of cool

--- app/src/main/java/io/github/hufghani/chirp/MainActivity.java
+++ app/src/main/java/io/github/hufghani/chirp/MainActivity.java
@@ -3,8 +3,15 @@ package io.github.hufghani.chirp;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
+import io.chirp.sdk.CallbackRead;
 import io.chirp.sdk.ChirpSDK;
+import io.chirp.sdk.ChirpSDKListener;
+import io.chirp.sdk.model.Chirp;
+import io.chirp.sdk.model.ChirpError;
+import io.chirp.sdk.model.ShortCode;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,13 +19,46 @@ public class MainActivity extends AppCompatActivity {
     final String API_SECRET  = "IdS6phZPntPpCbWmd2j7I5O5cs12gNM7mqkpoqcoUkY374gXT1";
     Context context;
     ChirpSDK chirpSDK;
-
+    TextView txtView = (TextView) findViewById(R.id.textView);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         context = this;
         chirpSDK = new ChirpSDK(context, API_KEY, API_SECRET);
+
+        chirpSDK.setListener(new ChirpSDKListener() {
+
+            @Override
+            public void onChirpHeard(ShortCode shortCode) {
+
+                chirpSDK.read(shortCode, new CallbackRead() {
+
+                    @Override
+                    public void onReadResponse(Chirp chirp) {
+                        txtView.setText(chirp.getJsonData().toString());
+                    }
+
+                    @Override
+                    public void onReadError(ChirpError chirpError) {
+
+                    }
+                });
+
+            }
+
+            @Override
+            public void onChirpError(ChirpError chirpError) {
+
+            }
+        });
+
+        chirpSDK.startListening();
+
+    }
+
+    public void test(View view) {
+        chirpSDK.play(new ShortCode("parrotbill"));
     }
 
 }
